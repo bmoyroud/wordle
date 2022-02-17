dc5cd1b1564f2e1a02db86c547df3fef49b4f4e5
bmoyroud <bastienm95@gmail.com>
2022-02-17 18:34:06 +0100

    Rename rows to empties

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -9,14 +9,14 @@ export default function Grid() {
   const [attempts] = useState([]);
 
   const completed = attempts.map(() => <CompletedRow />);
-  const rows = new Array(MAX_ATTEMPTS).fill().map(() => <EmptyRow />);
+  const empties = new Array(MAX_ATTEMPTS).fill().map(() => <EmptyRow />);
 
   return (
     <table>
       <tbody>
         {completed}
         <CurrentRow />
-        {rows}
+        {empties}
       </tbody>
     </table>
   );
