f21e4c9ae584806da5ec9ad72cd830a49e01a422
bmoyroud <bastienm95@gmail.com>
2022-02-17 18:33:53 +0100

    Add completed rows

--- src/components/grid/Grid.js
+++ src/components/grid/Grid.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import { MAX_ATTEMPTS } from '../../config';
 import CompletedRow from './CompletedRow';
@@ -6,12 +6,15 @@ import CurrentRow from './CurrentRow';
 import EmptyRow from './EmptyRow';
 
 export default function Grid() {
+  const [attempts] = useState([]);
+
+  const completed = attempts.map(() => <CompletedRow />);
   const rows = new Array(MAX_ATTEMPTS).fill().map(() => <EmptyRow />);
 
   return (
     <table>
       <tbody>
-        <CompletedRow />
+        {completed}
         <CurrentRow />
         {rows}
       </tbody>
