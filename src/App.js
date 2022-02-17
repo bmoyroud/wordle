730adecb4fb5814a29de9fa88255c02ca7c2a18e
bmoyroud <bastienm95@gmail.com>
2022-02-17 18:47:04 +0100

    Add keyup listener

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import './style.css';
 
 import Header from './components/Header';
@@ -8,6 +8,12 @@ import Keyboard from './components/Keyboard';
 export default function App() {
   const [attempts] = useState(['chalk', 'loved']);
   const [currentWord] = useState('empt');
+
+  useEffect(() => {
+    const listener = (e) => console.log(e);
+    window.addEventListener('keyup', listener);
+    return () => window.removeEventListener('keyup', listener);
+  });
 
   return (
     <div>
