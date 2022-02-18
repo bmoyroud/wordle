d334f658487fde0a1ed61e0a26405bbf38dff92f
bmoyroud <bastienm95@gmail.com>
2022-02-18 11:06:26 +0100

    Add Key component & refactor

---
+++ src/components/keyboard/Key.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export default function Key({ value }) {
+  return <button className="key">{value}</button>;
+}
--- src/components/keyboard/Keyboard.js
+++ src/components/keyboard/Keyboard.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import Key from './Key';
 
 export default function Keyboard() {
   return (
     <div className="keyboard">
-      <button className="key">q</button>
-      <button className="key">w</button>
-      <button className="key">e</button>
-      <button className="key">r</button>
-      <button className="key">t</button>
-      <button className="key">y</button>
-      <button className="key">u</button>
-      <button className="key">i</button>
-      <button className="key">o</button>
-      <button className="key">p</button>
+      <Key value={'q'} />
+      <Key value={'w'} />
+      <Key value={'e'} />
+      <Key value={'r'} />
+      <Key value={'t'} />
+      <Key value={'y'} />
+      <Key value={'u'} />
+      <Key value={'i'} />
+      <Key value={'o'} />
+      <Key value={'p'} />
 
-      <button className="key">a</button>
-      <button className="key">s</button>
-      <button className="key">d</button>
-      <button className="key">f</button>
-      <button className="key">g</button>
-      <button className="key">h</button>
-      <button className="key">j</button>
-      <button className="key">k</button>
-      <button className="key">l</button>
+      <Key value={'a'} />
+      <Key value={'s'} />
+      <Key value={'d'} />
+      <Key value={'f'} />
+      <Key value={'g'} />
+      <Key value={'h'} />
+      <Key value={'j'} />
+      <Key value={'k'} />
+      <Key value={'l'} />
 
-      <button className="key">Enter</button>
-      <button className="key">z</button>
-      <button className="key">x</button>
-      <button className="key">c</button>
-      <button className="key">v</button>
-      <button className="key">b</button>
-      <button className="key">n</button>
-      <button className="key">m</button>
-      <button className="key">Del</button>
+      <Key value={'Enter'} />
+      <Key value={'z'} />
+      <Key value={'x'} />
+      <Key value={'c'} />
+      <Key value={'v'} />
+      <Key value={'b'} />
+      <Key value={'n'} />
+      <Key value={'m'} />
+      <Key value={'Del'} />
     </div>
   );
 }
