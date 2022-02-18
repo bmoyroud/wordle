cec349774739f4a0f182b8ded1fce6b5d85f5e26
bmoyroud <bastienm95@gmail.com>
2022-02-18 11:16:11 +0100

    Refactor Keyboard

--- src/components/keyboard/Keyboard.js
+++ src/components/keyboard/Keyboard.js
@@ -15,38 +15,38 @@ export default function Keyboard({ onChar, onDelete, onEnter }) {
     }
   };
 
-  return (
-    <div className="keyboard">
-      <Key value={'q'} onClick={onClick} />
-      <Key value={'w'} onClick={onClick} />
-      <Key value={'e'} onClick={onClick} />
-      <Key value={'r'} onClick={onClick} />
-      <Key value={'t'} onClick={onClick} />
-      <Key value={'y'} onClick={onClick} />
-      <Key value={'u'} onClick={onClick} />
-      <Key value={'i'} onClick={onClick} />
-      <Key value={'o'} onClick={onClick} />
-      <Key value={'p'} onClick={onClick} />
+  const values = [
+    'q',
+    'w',
+    'e',
+    'r',
+    't',
+    'y',
+    'u',
+    'i',
+    'o',
+    'p',
+    'a',
+    's',
+    'd',
+    'f',
+    'g',
+    'h',
+    'j',
+    'k',
+    'l',
+    'enter',
+    'z',
+    'x',
+    'c',
+    'v',
+    'b',
+    'n',
+    'm',
+    'del',
+  ];
 
-      <Key value={'a'} onClick={onClick} />
-      <Key value={'s'} onClick={onClick} />
-      <Key value={'d'} onClick={onClick} />
-      <Key value={'f'} onClick={onClick} />
-      <Key value={'g'} onClick={onClick} />
-      <Key value={'h'} onClick={onClick} />
-      <Key value={'j'} onClick={onClick} />
-      <Key value={'k'} onClick={onClick} />
-      <Key value={'l'} onClick={onClick} />
+  const keys = values.map((val) => <Key value={val} onClick={onClick} />);
 
-      <Key value={'Enter'} onClick={onClick} />
-      <Key value={'z'} onClick={onClick} />
-      <Key value={'x'} onClick={onClick} />
-      <Key value={'c'} onClick={onClick} />
-      <Key value={'v'} onClick={onClick} />
-      <Key value={'b'} onClick={onClick} />
-      <Key value={'n'} onClick={onClick} />
-      <Key value={'m'} onClick={onClick} />
-      <Key value={'Del'} onClick={onClick} />
-    </div>
-  );
+  return <div className="keyboard">{keys}</div>;
 }
