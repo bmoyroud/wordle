c23258956d1dfcc65a3f7adec2ba46d46d525c00
bmoyroud <bastienm95@gmail.com>
2022-02-18 11:17:05 +0100

    Pass value to onClick

--- src/components/keyboard/Keyboard.js
+++ src/components/keyboard/Keyboard.js
@@ -2,10 +2,7 @@ import React from 'react';
 import Key from './Key';
 
 export default function Keyboard({ onChar, onDelete, onEnter }) {
-  const onClick = (e) => {
-    const key = e.target.innerText;
-    console.log(key);
-
+  const onClick = (key) => {
     if (key === 'Del') {
       onDelete();
     } else if (key === 'Enter') {
@@ -46,7 +43,9 @@ export default function Keyboard({ onChar, onDelete, onEnter }) {
     'del',
   ];
 
-  const keys = values.map((val) => <Key value={val} onClick={onClick} />);
+  const keys = values.map((val) => (
+    <Key value={val} onClick={() => onClick(val)} />
+  ));
 
   return <div className="keyboard">{keys}</div>;
 }
